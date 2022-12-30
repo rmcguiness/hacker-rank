46dd89d2476960d94303c02676fb966d55cb6d22
Ryan McGuiness <ryanmcguiness123@gmail.com>
2022-12-30 17:05:07 -0500

    new problem

--- problemSolving/sub_array_division.py
+++ problemSolving/sub_array_division.py
@@ -49,3 +49,24 @@ if __name__ == '__main__':
     fptr.write(str(result) + '\n')
 
     fptr.close()
+
+
+
+
+# Lily decides to share a contiguous segment of the bar selected such that:
+
+# The length of the segment matches Ron's birth month, and,
+# The sum of the integers on the squares is equal to his birth day.
+# Determine how many ways she can divide the chocolate.
+
+# Function Description
+# Complete the birthday function in the editor below.
+# birthday has the following parameter(s):
+
+# int s[n]: the numbers on each of the squares of chocolate
+# int d: Ron's birth day
+# int m: Ron's birth month
+
+
+# Returns
+# int: the number of ways the bar can be divided
