a637325c05a711d5914914ce9fe13d0b27989ecb
Ryan McGuiness <ryanmcguiness123@gmail.com>
2023-01-03 16:50:40 -0500

    daily problem

---
+++ problemSolving/divisible_sum_pairs.py
@@ -0,0 +1,61 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'divisibleSumPairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY ar
+#
+
+def divisibleSumPairs(n, k, ar):
+    # Write your code here
+    count = 0 
+    for i in range(n):
+        for j in range(i+1,n):
+            a = ar[i] + ar[j]
+            if a%k==0:
+                count += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+# Function Description
+
+# Complete the divisibleSumPairs function in the editor below.
+
+# divisibleSumPairs has the following parameter(s):
+
+# int n: the length of array 
+# int ar[n]: an array of integers
+# int k: the integer divisor
+# Returns
+# - int: the number of pairs
+
+# Input Format
+
+# The first line contains 2 space-separated integers, n and k.
+# The second line contains n space-separated integers, each a value of arr[i]
